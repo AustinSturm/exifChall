f4dc182a8481f24e1dadbc3d2c161543d13b2267
STURA019 <austin.d.sturm@disney.com>
2019-03-18 18:49:22 -0400

    psql added, shared volumes for psql and media

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
 # pillow dependencies  https://github.com/python-pillow/Pillow/issues/1763
-RUN apk add build-base python-dev py-pip jpeg-dev zlib-dev
+RUN apk add build-base python-dev py-pip jpeg-dev zlib-dev musl-dev postgresql-dev
 
 # set work directory
 WORKDIR /usr/src/exif
@@ -21,3 +21,5 @@ RUN pip install -r /usr/src/exif/requirements.txt
 
 # copy project
 COPY . /usr/src/exif/
+
+ENTRYPOINT ["/usr/src/exif/entrypoint.sh"]
--- exif/exif/settings.py
+++ exif/exif/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = 'exif.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# settings.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
     }
 }
 
